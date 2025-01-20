660ac1ae647e80482ff8a070c0a8593a18c4c290
elixireous <87973863+elixireous@users.noreply.github.com>
2025-01-20 16:07:17 +0400

    Update ext.py

--- ext.py
+++ ext.py
@@ -17,7 +17,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
-
+app.config['DEBUG'] = False
+app.config['ENV'] = 'production'
 
 def load_user(user_id):
     from models import User
